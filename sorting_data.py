7406f7c2668cc2a19f77cd8255461c7fcfb49348
PierigS <pierig.servouze@gmail.com>
2024-10-25 13:06:19 +0200

    adding scored goals to stats

--- sorting_data.py
+++ sorting_data.py
@@ -16,72 +16,85 @@ for game in data['games']:
     for team in ['home', 'away']:
         team_name = game[f"{team}_team"]['namecode']
         if team_name not in clubs:
-            clubs[team_name] = {'games': [], 'means': {}}
+            clubs[team_name] = {'games': [], 'means': {'overview':{'goalsScored':{'total':0, 'count':0}, 'goalsAgainst': {'total':0, 'count':0}}}}
             clubs[team_name]['name'] = game[f"{team}_team"]['name']
 
 for game in data['games']:
     for team, opponent in [('home', 'away'), ('away', 'home')]:
-        team_name = game[f"{team}_team"]['namecode']
-        opponent_name = game[f"{opponent}_team"]['namecode']
-        
-        team_game = {
-            'isHome': team,
-            'score': game['score'],
-            'stats': {},
-        }
-        
-        if 'statistics' in game:
-            for stats_group in game['statistics']:
-                # Initialiser le groupe de statistiques s'il n'existe pas encore
-                if stats_group not in team_game['stats']:
-                    team_game['stats'][stats_group] = {}
-                
-                for stat, stat_data in game['statistics'][stats_group].items():
-                    value = stat_data[team]
-                    team_game['stats'][stats_group][stat] = {
-                        'value': value,
-                        'type': stat_data['type']
-                    }
+        if game['score'] != 'postponed':
+            team_name = game[f"{team}_team"]['namecode']
+            opponent_name = game[f"{opponent}_team"]['namecode']
+            
+            team_game = {
+                'isHome': team,
+                'score': game['score'],
+                'stats': {},
+            }
+            # Ajouter les buts marqués et encaissés
+            if team == 'home':
+                goals_scored = game['score']['home']
+                goals_against = game['score']['away']
+            else:
+                goals_scored = game['score']['away']
+                goals_against = game['score']['home']
+            
+            clubs[team_name]['means']['overview']['goalsScored']['total'] += goals_scored
+            clubs[team_name]['means']['overview']['goalsScored']['count'] += 1
+            clubs[team_name]['means']['overview']['goalsAgainst']['total'] += goals_against
+            clubs[team_name]['means']['overview']['goalsAgainst']['count'] += 1
 
-                    # Ajouter la statistique au total pour les moyennes
-                    if stats_group not in clubs[team_name]['means']:
-                        clubs[team_name]['means'][stats_group] = {}
-
-                    if stat not in clubs[team_name]['means'][stats_group]:
-                        clubs[team_name]['means'][stats_group][stat] = {
-                            'total': 0, 
-                            'count': 0,
-                        }
-                    clubs[team_name]['means'][stats_group][stat]['total'] += value
-                    clubs[team_name]['means'][stats_group][stat]['count'] += 1
-
-                    # Ajouter les statistiques subies selon `stat_against`
-                    if stats_group in stat_against and stat in stat_against[stats_group]:
-                        new_name = stat_against[stats_group][stat]['new_name']
-                        new_group = stat_against[stats_group][stat]['new_group']
-                        opponent_value = stat_data[opponent]
-                        
-                        if new_group not in team_game['stats']:
-                            team_game['stats'][new_group] = {}
-                        
-                        team_game['stats'][new_group][new_name] = {
-                            'value': opponent_value,
+            if 'statistics' in game:
+                for stats_group in game['statistics']:
+                    # Initialiser le groupe de statistiques s'il n'existe pas encore
+                    if stats_group not in team_game['stats']:
+                        team_game['stats'][stats_group] = {}
+                    
+                    for stat, stat_data in game['statistics'][stats_group].items():
+                        value = stat_data[team]
+                        team_game['stats'][stats_group][stat] = {
+                            'value': value,
                             'type': stat_data['type']
                         }
 
-                        # Ajouter les statistiques subies aux moyennes
-                        if new_group not in clubs[team_name]['means']:
-                            clubs[team_name]['means'][new_group] = {}
+                        # Ajouter la statistique au total pour les moyennes
+                        if stats_group not in clubs[team_name]['means']:
+                            clubs[team_name]['means'][stats_group] = {}
 
-                        if new_name not in clubs[team_name]['means'][new_group]:
-                            clubs[team_name]['means'][new_group][new_name] = {
+                        if stat not in clubs[team_name]['means'][stats_group]:
+                            clubs[team_name]['means'][stats_group][stat] = {
                                 'total': 0, 
                                 'count': 0,
                             }
-                        clubs[team_name]['means'][new_group][new_name]['total'] += opponent_value
-                        clubs[team_name]['means'][new_group][new_name]['count'] += 1
+                        clubs[team_name]['means'][stats_group][stat]['total'] += value
+                        clubs[team_name]['means'][stats_group][stat]['count'] += 1
 
-        clubs[team_name]['games'].append(team_game)
+                        # Ajouter les statistiques subies selon `stat_against`
+                        if stats_group in stat_against and stat in stat_against[stats_group]:
+                            new_name = stat_against[stats_group][stat]['new_name']
+                            new_group = stat_against[stats_group][stat]['new_group']
+                            opponent_value = stat_data[opponent]
+                            
+                            if new_group not in team_game['stats']:
+                                team_game['stats'][new_group] = {}
+                            
+                            team_game['stats'][new_group][new_name] = {
+                                'value': opponent_value,
+                                'type': stat_data['type']
+                            }
+
+                            # Ajouter les statistiques subies aux moyennes
+                            if new_group not in clubs[team_name]['means']:
+                                clubs[team_name]['means'][new_group] = {}
+
+                            if new_name not in clubs[team_name]['means'][new_group]:
+                                clubs[team_name]['means'][new_group][new_name] = {
+                                    'total': 0, 
+                                    'count': 0,
+                                }
+                            clubs[team_name]['means'][new_group][new_name]['total'] += opponent_value
+                            clubs[team_name]['means'][new_group][new_name]['count'] += 1
+
+            clubs[team_name]['games'].append(team_game)
 
 # Calculer les moyennes pour chaque statistique
 for club in clubs:
